30649de0a870279576a67ed63122a8bd36227d43
Peng Fong Er <peng-fong.er@epf.my>
2025-10-16 11:28:28 +0800

    Updated builds

--- precision/core.pyi
+++ precision/core.pyi
@@ -8,6 +8,7 @@ from pymc_marketing import clv
 from rfm_analyzer import RFMAnalyzer
 from typing import Any
 import logging
+import warnings
 
 def run_customer_segmentation(customers_df: Any, orders_df: Any, analysis_date: Any) -> Any:
     ...
@@ -32,3 +33,4 @@ import pymc_extras.prior
 import pymc_marketing
 import pymc_marketing.clv
 import logging
+import warnings
