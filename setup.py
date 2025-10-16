dd8c7a1f26f4730201d343b0cd3d3a7d750a08a0
Peng Fong Er <peng-fong.er@epf.my>
2025-10-16 10:11:15 +0800

    Updated package

--- setup.py
+++ setup.py
@@ -5,4 +5,11 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     package_data={"precision": ["*.so"]},
+    # required dependencies
+    install_requires=[
+        "matplotlib",
+        "pandas",
+        "pymc_marketing",
+        "seaborn"
+    ],
 )
